1b4e4c86e8e359d4c26f8e54ce4c4cbe65e13cde
Arid009 <ariddasgupta@gmail.com>
2025-01-13 14:45:44 +0600

    its done

---
+++ app.js
@@ -0,0 +1,132 @@
+
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=a`)
+    .then(res => res.json())
+    .then(data => {
+        clearCard();
+        displayProd(data.meals);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+document.getElementById("search").addEventListener("click", (event) => {
+    const inputVal = document.getElementById("inputVal").value;
+    console.log(inputVal);
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputVal}`)
+    .then(res => res.json())
+    .then(data => {
+        clearCard();
+        displayProd(data.meals);
+    })
+    .catch((err) => {
+        console.log(err);
+        if (err) {
+            const cartCont = document.getElementById("card-cont");
+            cartCont.innerHTML=`
+                <div>
+                <h1 class="text-2xl text-center font-medium">Nof found</h1>
+                </div>
+            `;
+            console.log("dbie");
+        }
+        
+    })
+
+
+})
+
+const clearCard = ()=>{
+    const cartCont = document.getElementById("card-cont");
+    cartCont.innerHTML='';
+}
+
+const displayProd = (prods) => {
+    const cartCont = document.getElementById("card-cont");
+    prods.forEach(prod => {
+
+
+        const div = document.createElement("div");
+        div.innerHTML =
+            `
+            <div class="card" style="width: 260px;">
+                <img src="${prod.strMealThumb}" class="card-img-top w-64 h-52 " alt="...">
+                <div class="card-body text-center">
+                    <h5 class="text-lg font-medium">Name: ${prod.strMeal}</h5>
+                    <p class="card-text">Category: ${prod.strCategory}</p>
+                    <p class="card-text my-2">Instructions: ${prod.strInstructions.slice(0, 15)}</p>
+                    <!-- Button trigger modal -->
+                    <button onclick="singleProd(${prod.idMeal})" class="btn btn-primary" >
+                    Details
+                    </button>
+
+
+                    
+                    <button onclick="handleAddToCart('${prod.strMeal}')" class="btn btn-primary">Add To Cart</button>
+                </div>
+            </div>
+                    `
+
+        cartCont.appendChild(div);
+    });
+}
+const singleProd = (id) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((res) => res.json())
+    .then((json) => set(json.meals[0]))
+    
+    function set(prod) {
+        const title = document.getElementById("mod-title");
+        const body = document.getElementById("mod-body");
+        console.log(prod);
+        
+        title.innerText = prod.strMeal;
+        body.innerHTML = `
+        <img src="${prod.strMealThumb}" class="card-img-top m-auto w-64 h-52" alt="${prod.strMeal}">
+        <p class="text-xl my-3 font-bold">Category: ${prod.strCategory}</p>
+        <p class="text-xl my-3">Area: ${prod.strArea}</p>
+        <p class="text-justify">Instructions: ${prod.strInstructions}</p>
+        `;
+    }
+    let modal1 = new bootstrap.Modal(document.getElementById("nothing"));
+
+    function toggleModal1() {
+        modal1.toggle();
+    }
+    toggleModal1();
+}
+const handleAddToCart = (item) => {
+    const cartCount = document.getElementById("count").innerText;
+    // console.log(item)
+    let convertedCount = parseInt(cartCount)
+    if (convertedCount > 6) {
+        return alert("Can't add item");
+    }
+    convertedCount++;
+    document.getElementById("count").innerText = convertedCount;
+    const container = document.getElementById("table");
+    const tr = document.createElement("tr");
+    // console.log(item.strMeal);
+    tr.innerHTML = `
+                <th scope="row">${convertedCount}</th>
+                <td>${item}</td>
+    `;
+    container.appendChild(tr);
+}
+
+
+
+
+
+// function set(prod) {
+//     const title = document.getElementById("mod-title");
+//     const body = document.getElementById("mod-body");
+//     console.log(prod);
+    
+//     title.innerText = prod.strMeal;
+//     body.innerHTML = `
+//     <img src="${prod.strMealThumb}" class="card-img-top m-auto w-64 h-52" alt="${prod.strMeal}">
+//     <p class="text-xl my-3 font-bold">Category: ${prod.strCategory}</p>
+//     <p class="text-xl my-3">Area: ${prod.strArea}</p>
+//     <p class="text-justify">Instructions: ${prod.strInstructions}</p>
+// `;
